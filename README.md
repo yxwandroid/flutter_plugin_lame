838f6b8c0b17c9ba8fd174381bdce87bc8623cee
wilson <>
2020-11-06 17:56:50 +0800

    添加ios代码

--- README.md
+++ README.md
@@ -6,11 +6,23 @@ Flutter 录制MP3插件  使用lame进行录制.支持Android和IOS
 
 ### android集成注意事项
 
-   xmlns:tools="http://schemas.android.com/tools"
-
-   tools:replace="android:label"
+android如下图添加所示
+   ![](.images/android集成步骤1.png)
+   
+    在指定位置添加如下内容
+    xmlns:tools="http://schemas.android.com/tools"
+       tools:replace="android:label"
+   
+   
+   
+   
+   
+   
+   
    
  
-   ![](images/android集成步骤1.png)
+
+
+### IOS集成注意事项
 
 
--- ios/Classes/SwiftFlutterPluginLamePlugin.swift
+++ ios/Classes/SwiftFlutterPluginLamePlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import LameTool
 
 public class SwiftFlutterPluginLamePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -7,8 +8,63 @@ public class SwiftFlutterPluginLamePlugin: NSObject, FlutterPlugin {
     let instance = SwiftFlutterPluginLamePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
-
+  let recoder_manager = RecordManager()//初始化
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+     if call.method=="startRecord"{
+        startRecord()
+        result("startRecord")
+    }else if call.method=="stopRecord"{
+        stopRecord()
+        result("stopRecord")
+    }else if call.method=="playRecord"{
+        playRecord()
+        let filePath = ""
+        result(filePath)
+    }
+   
   }
+
+    
+     func startRecord() {
+        recoder_manager.beginRecord()//开始录音
+    }
+    
+     func stopRecord() {
+        recoder_manager.stopRecord()//结束录音
+    }
+     func playRecord() {
+        recoder_manager.play()//播放录制的音频
+    }
+
+     func record(_ sender: UIButton) {
+        let input = Bundle.main.path(forResource: "file_example_WAV_10MG", ofType: "wav")!
+        let output = FileManager.default.temporaryFileURL(fileName: "\(UUID().uuidString).mp3")!
+
+        AudioConverter.encodeToMp3(
+            inPcmPath: input,
+            outMp3Path: output.path,
+            onProgress: { [unowned self] progress in
+                print( "Progress: \(Int(100 * progress))")
+            }, onComplete: {
+                print(output.path)
+                print( "Complete")
+        })
+    }
 }
+
+
+
+extension FileManager {
+
+    func temporaryFileURL(fileName: String = UUID().uuidString) -> URL? {
+        return URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent(fileName)
+    }
+}
+
+infix operator -->
+// prepare class instance
+func --> <T>(object: T, closure: (T) -> Void) -> T {
+    closure(object)
+    return object
+}
+
